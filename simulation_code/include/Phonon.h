e9f231c632a4a27aebf9cb4e34df217c8e70421a
GwG <32935379+GwGibson@users.noreply.github.com>
2022-12-18 17:51:49 -0500

    Update Phonon.h

--- simulation_code/include/Phonon.h
+++ simulation_code/include/Phonon.h
@@ -10,7 +10,7 @@ class Cell;
 
 class Phonon {
 public:
-    constexpr std::size_t NUM_RELAX_RATES = 3;
+    static constexpr std::size_t NUM_RELAX_RATES = 3;
     enum class Polarization { LA, TA };
 
     using RelaxRates = std::array<double, NUM_RELAX_RATES>;
